4675e5db609bc64c4c50b9a6ab0e2e7297a59fc8
leongus1 <leongus1@gmail.com>
2020-10-04 20:14:43 -0700

    working website. needs timezone detection

--- show_time_app/views.py
+++ show_time_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from time import gmtime, strftime
+from django.utils import timezone
+import datetime
+
+
+
+def show_time(request):
+    context = {
+        "time": strftime("%B %d, %Y \n%H:%M %p", gmtime())
+    }
+    return render(request, 'index.html', context)
 
 # Create your views here.
